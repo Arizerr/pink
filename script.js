37e3afd378a9d6c484a374f5d6480e02254753e5
Ari <50357852+Arizerr@users.noreply.github.com>
2025-02-05 02:04:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const messages = [
+/*const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
@@ -9,6 +9,19 @@ const messages = [
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
+];*/
+
+const messages = [
+    "km yakin nih? 🥺",
+    "beneran yakin?? 😳",
+    "fix nggak berubah pikiran? 🤨",
+    "sayang plis... 🥹💖",
+    "coba pikirin lagi sayang! 🤔✨",
+    "kalau km ga sayang, aku bakal sedih banget... 😭",
+    "ak jadi sediiih banget... 😭💔",
+    "ak jadi sedih banget banget banget... 😭💔💔💔",
+    "yaudah deh, ak berhenti nanya... 😞",
+    "ehhh boong, bilang iya dong plsss! ❤️🥰"
 ];
 
 let messageIndex = 0;
